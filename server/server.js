5c04e4d449bd09ee52382594ad8540663e66c5f9
unknown <timaZhuk>
2026-08-15 21:51:12 +0500

    changed server.js logging message

--- server/server.js
+++ server/server.js
@@ -53,7 +53,7 @@ mongoose
 
 // Graceful shutdown
 process.on('SIGINT', async () => {
-  console.log('Changed again to - Shutting down gracefully...');
+  console.log('Changed again to - Shutting down on working server...');
   await mongoose.connection.close();
   process.exit(0);
 });
